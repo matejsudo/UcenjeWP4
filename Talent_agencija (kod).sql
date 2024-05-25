b44b96bfdacc56b1297190cf8fc7016be99bd3ba
matejsudo <168752302+matejsudo@users.noreply.github.com>
2024-05-25 10:14:11 +0200

    Inserti dodani u originalni kod

--- Talent_agencija (kod).sql
+++ Talent_agencija (kod).sql
@@ -1,4 +1,4 @@
-use master;
+﻿use master;
 GO
 drop database if exists talent_agencija;
 go
@@ -25,7 +25,7 @@ profesija varchar(100),
 datumrodenja datetime,
 korisnikod datetime,
 email varchar(50),
-agent int not null
+agent int
 );
 
 create table projekti(
@@ -47,7 +47,51 @@ alter table korisnicinaprojektu add foreign key (korisnik) REFERENCES korisnici(
 alter table korisnicinaprojektu add FOREIGN KEY (projekt) references projekti(sifra);
 
 select * from agenti;
+
 select * from korisnici;
+
 select * from projekti;
+
 select * from korisnicinaprojektu;
  
+insert into agenti(ime,prezime) values
+('Mirko', 'Birko'), ('Dinko', 'Parkov');
+
+insert into korisnici(ime, prezime, email) values
+('Anka', 'Šumadinov', 'ankasum@gmail.com'),
+('Senka', 'Pokrov', 'senkap@gmail.com'), 
+('Igor','Balažić','igorbalazic11@gmail.com'),
+('Damir','Bukvašević','damirbukvasevic@gmail.com'),
+('Senka','Banjac','sbanjac88@gmail.com'),
+('Igor','Brzica','igorbrzica1@gmail.com'),
+('Ivan','Mesarić','mesaritchievan@gmail.com'),
+('Mirko','Ereš','mirko.eres1@gmail.com'),
+('Lea','Raguž','raguz.leaa@gmail.com'),
+('Dominik  ','Daraždi','dominikdarazdi@gmail.com'),
+('Dean','Vagner','deanvagner@yahoo.com'),
+('Marko','Mudrovčić','markomudrovcic12@gmail.com'),
+('Mario','Kušen','kusen.mario@gmail.com'),
+('Leon','Jalovičar','ringerajacar@gmail.com'),
+('Dražen','Kalazić','thekalazic@gmail.com'),
+('Matej','Sudarić','matejsudo@gmail.com'),
+('Antonijo','Šakić','a.sakic.1.g@gmail.com'),
+('Nikola','Milić','nikk.mil@gmail.com'),
+('Manuela','Slobođanac','manja496@gmail.com'),
+('Srđan','Filipović','srdjanfilipovic991@gmail.com'),
+('Goran','Matoš','goran.matos@a1.hr'),
+('Igor','Tomić','i.tomic.babinski@gmail.com'),
+('Borna','Štajduhar','bornastajduhar1999@gmail.com'),
+('Ante','Filipović','ante.filipovic72@gmail.com'),
+('Domagoj','Glavačević','glavacevic.d@gmail.com'),
+('Lorena','Ostroški','lorena.ostroski2503@gmail.com'),
+('Sanja','Boduljak','matkosanja@gmail.com'),
+('Leon','Puhanić','leonpuhanic@gmail.com'),
+('Danijel','Sačer','sacerdanijel@gmail.com');
+
+insert into projekti(naziv, vrijednostprojekta, produkcija) values
+('Reklama za Jamnicu', '500000.00', 'Antitalent'),
+('Spot za Donkeyhot', '7000.00', 'Kinofilm'),
+('Sapunica Villa Maria', '550000.00', 'NovaTV');
+
+insert into korisnicinaprojektu(projekt, korisnik) values
+(1,1), (1,4), (1,7), (1,8);
